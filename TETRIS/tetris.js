9abffcffb81896ba347e98a5bda419cf848be14c
Gregory Palasciano <greg.palasciano@gmail.com>
2020-09-16 22:38:59 -0700

    Update tetris.js

--- TETRIS/tetris.js
+++ TETRIS/tetris.js
@@ -8,7 +8,7 @@ context.scale(20, 20);
 // When you get Tetris
 function arenaSweep() {
   let rowCount = 1;
-  outer: for (let y = arena.length - 1; y > 0; --y) {
+  for (let y = arena.length - 1; y > 0; --y) {
     for (let x = 0; x < arena[y].length; ++x) {
       if (arena[y][x] === 0) {
         continue outer;
